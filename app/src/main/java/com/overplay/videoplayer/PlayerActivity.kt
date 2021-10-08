3b2c58fd64dcf61b5516dcd17c8e72144dab8891
David K. Lee <prelude618@gmail.com>
2021-10-09 02:37:58 +0900

    Implemented a basic player

---
+++ app/src/main/java/com/overplay/videoplayer/PlayerActivity.kt
@@ -0,0 +1,92 @@
+package com.overplay.videoplayer
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.overplay.videoplayer.databinding.ActivityPlayerBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.util.Util
+
+
+class PlayerActivity : AppCompatActivity() {
+    private var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition = 0L
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityPlayerBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT < 24 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(this)
+                .build()
+                .also { exoPlayer ->
+                    viewBinding.videoView.player = exoPlayer
+                    val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                    exoPlayer.setMediaItem(mediaItem)
+                    exoPlayer.playWhenReady = playWhenReady
+                    exoPlayer.seekTo(currentWindow, playbackPosition)
+                    exoPlayer.prepare()
+                }
+
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        viewBinding.videoView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun releasePlayer() {
+        player?.run {
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentWindowIndex
+            playWhenReady = this.playWhenReady
+            release()
+        }
+        player = null
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+}
